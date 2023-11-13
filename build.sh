e7643a125a0f48b169ab8054ef36cd2edebe9b11
AntonioBerna <asukeasukai12@gmail.com>
2023-11-13 04:28:27 +0100

    improve building of project :tada:

--- build.sh
+++ build.sh
@@ -1,6 +1,33 @@
-# !/bin/sh
+#!/bin/sh
 
-set -xe
+dir="debug/"
+founds="founds/"
+target="gh-code-finder.app"
 
-cmake .
-make
+if [ "$#" -eq 0 ]; then
+    set -xe
+    mkdir "$dir" && cd "$dir"
+    cmake ..
+    make
+    mv "$target" ../
+    echo "Build complete."
+    exit 0
+elif [ "$#" -eq 1 ] && [ "$1" == "clear" ]; then
+    if [ -d "$dir" ]; then
+        rm -r "$dir"
+    fi
+
+    if [ -d "$founds" ]; then
+        rm -r "$founds"
+    fi
+
+    if [ -e "$target" ]; then
+        rm "$target"
+    fi
+
+    echo "Done."
+    exit 0
+else
+    echo "Usage: sh $0 <options=[clear,]>"
+    exit 1
+fi
